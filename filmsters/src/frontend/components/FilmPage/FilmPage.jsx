b1ab76cbea473cd20623e6769d969b6e033bf56a
katalinapalma <kata_92@hotmail.com>
2020-05-22 13:04:24 +0200

    installed react icons

--- filmsters/src/frontend/components/FilmPage/FilmPage.jsx
+++ filmsters/src/frontend/components/FilmPage/FilmPage.jsx
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Card from 'react-bootstrap/Card'
+
 import Navigation from '../Navigation/Navigation';
+import styles from '../FilmPage/FilmPage.module.css';
 
 class FilmPage extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <Navigation />
-        <h1>Filmz</h1>
-      </div>
+        <div className={styles.movieContainer}>
+          <div className={styles.divLeft}> 
+            <Card style={{ width: '30rem' }}>
+              <Card.Img variant="top" src="https://d2iltjk184xms5.cloudfront.net/uploads/photo/file/280892/654b1c569a9a2b850e8d9c4de3bd8f02-Inception-movie-poster-7.jpg" />
+            </Card>
+          </div>
+          <div className={styles.divRight}>
+            <h1>Inception (2010)</h1>
+          </div>
+        </div>
+      </Fragment>
     )
   }
 }
