351f2649ae675434bcf4bafc50c87a991174e8f5
AboveSpring <holmberg_joel@outlook.com>
2020-05-25 13:29:30 +0200

    reg done with val

--- filmsters/src/frontend/components/Registration.jsx
+++ filmsters/src/frontend/components/Registration.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Modal, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import withApiRequests from './HOC/withApiRequests';
 
 class Registration extends Component {
   constructor(props){
@@ -8,15 +9,43 @@ class Registration extends Component {
 
     this.state = {
         showModal: true,
+        firstnameInput:{ value : '', valid : false },
+        lastnameInput:{ value : '', valid : false },
+        emailInput:{ value : '', valid : false },
+        passwordInput:{ value : '', valid : false },
+
     }
 }
 
+handleInputChange = (event) => {
+  this.setState({ [event.target.name]: { value: event.target.value, valid: !!event.target.value } });
+}
+
+addUser = (event) => {
+  event.preventDefault();
+
+  event.target.className += " was-validated";
+
+  if(this.state.firstnameInput.valid === true & this.state.lastnameInput.valid === true 
+    & this.state.emailInput.valid === true & this.state.passwordInput.valid === true){
+    
+    this.props.history.push('/');
+    this.newUser = {
+      firstname: this.state.firstnameInput.value,
+      lastname: this.state.lastnameInput.value,
+      email: this.state.emailInput.value,
+      password: this.state.passwordInput.value
+      }
+    this.props.postUser(this.newUser);}
+}
+
 render() {
+  const {  firstnameInput, emailInput, lastnameInput, passwordInput, showModal } = this.state;
   return (
     <div>
       <Modal
         size="lg"
-        show={this.state.showModal}
+        show={showModal}
         onHide={() => {
           this.setState({showModal: false})
       }}
@@ -27,29 +56,61 @@ render() {
         </Modal.Title>
         </Modal.Header>
         <Modal.Body style={{backgroundColor: 'white'}}>
-          <Form className="loginForm">
+          <Form noValidate className="loginForm" onSubmit={this.addUser}>
               
-            <Form.Group controlId="formBasicFirstName" className="loginForm">
+            <Form.Group controlId="formBasicFirstName" className="loginForm" >
                 <Form.Label className="loginForm">First Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter Firstname" />
+                <Form.Control  
+                  type='text'
+                  required
+                  name="firstnameInput"
+                  value={firstnameInput.value}
+                  onChange={this.handleInputChange} 
+                  placeholder="Enter Firstname" />
+                <div className="valid-feedback">Looks good!</div>
             </Form.Group>
+            
 
             <Form.Group controlId="formBasicLastName" className="loginForm">
                 <Form.Label className="loginForm">Last Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter Lastname" />
+                <Form.Control 
+                  type='text'
+                  name="lastnameInput"
+                  required
+                  value={lastnameInput.value}
+                  onChange={this.handleInputChange} 
+                  placeholder="Enter Lastname" />
+                <div className="valid-feedback">Looks good!</div>
             </Form.Group>
+            
 
 
             <Form.Group controlId="formBasicEmail" className="loginForm">
                 <Form.Label className="loginForm">Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control 
+                  type='email'
+                  name="emailInput"
+                  value={emailInput.value}
+                  required
+                  onChange={this.handleInputChange}
+                  placeholder="Enter email" />
+                <div className="valid-feedback">Looks good!</div>  
             </Form.Group>
 
             <Form.Group className="loginForm" controlId="formBasicPassword">
                 <Form.Label className="loginForm">Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control 
+                  type="password"
+                  name="passwordInput"
+                  required
+                  value={passwordInput.value}
+                  onChange={this.handleInputChange} 
+                  placeholder="Password" />
+                <div className="valid-feedback">Looks good!</div>  
             </Form.Group>
-            <Button variant="primary" type="submit">
+
+
+            <Button variant="primary" type="submit"  >
                 Create
             </Button>
             <Form.Text className="loginForm">Already have an account? <Link to="/login">Login here!</Link></Form.Text>
@@ -62,4 +123,4 @@ render() {
   }
 }
 
-export default Registration;
+export default withApiRequests (Registration);
